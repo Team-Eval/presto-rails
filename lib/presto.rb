80bee012a7d82f482e16b16f7df28d8f9f903fca
Logan Hasson <logan.hasson@gmail.com>
2013-11-06 16:20:37 -0500

    Add error handling when project names contain spaces

--- lib/presto.rb
+++ lib/presto.rb
@@ -2,8 +2,8 @@ class Presto
 
   PRESTO_ROOT = File.expand_path('../', File.dirname(__FILE__))
   def self.run
-    if ARGV[0].nil? || ['-h','--help'].include?(ARGV[0])
-      puts <<-HELP
+    if ARGV[0].nil? || ['-h','--help'].include?(ARGV[0]) || ARGV[1]
+      puts <<-HELP.gsub(/^ {6}/, '')
       Usage:
         presto <app_name>
       HELP
